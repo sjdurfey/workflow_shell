5e8c8592602eeacccbe2980452423b4c1ee32abd
Mac Champion <mac.champion@cerner.com>
2015-08-19 08:56:46 -0500

    test commit, please squash

--- src/WorkflowShell/hadoop/hdl.sh
+++ src/WorkflowShell/hadoop/hdl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function printHDLhelp {
-    echo "usage: wsh hdl <job ID> <URL to job>"
+    echo "Usage: wsh hdl <job ID> <URL to job>"
     echo "Example: wsh hdl \"201507281713_19288\" \"http://pophdevhadoopmstr01.northamerica.cerner.net:50030/jobdetails.jsp?jobid=job_201507281713_19288\""
 }
 
--- src/WorkflowShell/misc/uploadjars.sh
+++ src/WorkflowShell/misc/uploadjars.sh
@@ -11,8 +11,11 @@ function uploadjars {
     fi
 
     #TODO: exclude those "original" jars
+    rm *-console/target/*orig*.jar || true
+    rm target/*orig*.jar || true
+    scp *-console/target/*console*SNAPSHOT.jar "$USER@pophdevvm219:~/" || true
+    scp target/*console*SNAPSHOT.jar "$USER@pophdevvm219:~/" || true
     scp *-console/target/*console*SNAPSHOT.jar "$USER@pophdevutil30:~/" || true
     scp target/*console*SNAPSHOT.jar "$USER@pophdevutil30:~/" || true
-    scp *-console/target/*console*SNAPSHOT.jar "$USER@pophdevutil29:~/" || true
-    scp target/*console*SNAPSHOT.jar "$USER@pophdevutil29:~/" || true
+
 }
